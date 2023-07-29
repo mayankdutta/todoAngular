3ff8ac3edd5ed7adc2c2bf8bd421243a5423f26a
mayank dutta <smayankdutt@gmail.com>
2023-07-29 19:12:46 +0530

    sorting corrected

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -21,16 +21,24 @@ export class TodoService {
   }
 
   updateTask(id: number, modifyingData: any) {
-    this.todoList = this.todoList.map(value=> {
+    this.todoList = this.todoList.map((value) => {
       if (value.id === id) {
-        value.title = modifyingData.title ? modifyingData.title :  value.title;
-        value.description = modifyingData.description ? modifyingData.description :  value.description;
-        value.priority = modifyingData.priority ? modifyingData.priority :  value.priority;
-        value.status = modifyingData.status ? modifyingData.status :  value.status;
-        value.dueDate = modifyingData.dueDate ? modifyingData.dueDate :  value.dueDate;
+        value.title = modifyingData.title ? modifyingData.title : value.title;
+        value.description = modifyingData.description
+          ? modifyingData.description
+          : value.description;
+        value.priority = modifyingData.priority
+          ? modifyingData.priority
+          : value.priority;
+        value.status = modifyingData.status
+          ? modifyingData.status
+          : value.status;
+        value.dueDate = modifyingData.dueDate
+          ? modifyingData.dueDate
+          : value.dueDate;
       }
       return value;
-    })
+    });
 
     this.saveToLocalStorage();
     return this.todoList;
@@ -53,7 +61,6 @@ export class TodoService {
   }
 
   constructor() {
-    // Fetch data from localStorage when the service is instantiated
     const savedData = localStorage.getItem('todoList');
     if (savedData) {
       this.todoList = JSON.parse(savedData);
@@ -61,7 +68,6 @@ export class TodoService {
   }
 
   private saveToLocalStorage() {
-    // Save data to localStorage whenever the todoList is updated
     localStorage.setItem('todoList', JSON.stringify(this.todoList));
   }
 
@@ -72,23 +78,40 @@ export class TodoService {
     title: (elem1: iTodoItem, elem2: iTodoItem) => {
       if (elem1.title < elem2.title) return -1;
       if (elem1.title > elem2.title) return +1;
-      return 0;
+      return elem1.id - elem2.id;
     },
     priority: (elem1: iTodoItem, elem2: iTodoItem) => {
-      if (elem1.priority < elem2.priority) return -1;
-      if (elem1.priority > elem2.priority) return +1;
-      return 0;
+      const check = (attribute: string) => {
+        attribute = attribute.toLowerCase();
+        if (attribute === 'low') return 1;
+        if (attribute === 'medium') return 2;
+        return 3;
+      };
+
+      let priority1 = check(elem1.priority);
+      let priority2 = check(elem2.priority);
+
+      if (priority1 !== priority2) return priority1 - priority2;
+      return elem1.id - elem2.id;
     },
     status: (elem1: iTodoItem, elem2: iTodoItem) => {
-      if (elem1.status < elem2.status) return -1;
-      if (elem1.status > elem2.status) return +1;
-      return 0;
+      const check = (attribute: string) => {
+        attribute = attribute.toLowerCase();
+        if (attribute === 'done') return 3;
+        if (attribute === 'todo') return 2;
+        return 1;
+      };
+
+      let status1 = check(elem1.status);
+      let status2 = check(elem2.status);
+
+      if (status1 !== status2) return status1 - status2;
+      return elem1.id - elem2.id;
     },
     dueDate: (elem1: iTodoItem, elem2: iTodoItem) => {
       if (elem1.dueDate < elem2.dueDate) return -1;
       if (elem1.dueDate > elem2.dueDate) return +1;
-      return 0;
+      return elem1.id - elem2.id;
     },
   };
-
 }
